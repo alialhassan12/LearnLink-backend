8ba9d068b63e495c55c5bdfb77c2f8dc1f07f70d
alialhassan12 <alialhasan65t@gmail.com>
2026-07-04 11:07:24 +0300

    cleaned create course controller

--- app/Http/Controllers/Api/coursesController.php
+++ app/Http/Controllers/Api/coursesController.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Log;
 class coursesController extends Controller
 {
     public function createCourse(Request $request,SupabaseStorageService $storage,SubscriptionService $subscriptionService){
-        Log::info("Enter course creation request");
         $request->validate([
             "category_id"=>"required|exists:categories,id",
             "title"=>"required|string",
@@ -38,8 +37,6 @@ class coursesController extends Controller
             "sections.*.materials.*.file"=>"required|file",
             "sections.*.materials.*.size"=>"required|integer",
         ]);
-
-        Log::info("Passed course validation");
 
         $user=$request->user();
         if(!$user){
@@ -64,20 +61,15 @@ class coursesController extends Controller
             ],403);
         }
 
-        Log::info("Passed authorization checks");
-
         $course=DB::transaction(function() use ($request,$teacher,$storage){
-            Log::info("Inside transaction");
             $thumbnailPath=$storage->uploadthumbnail(
                 $request->thumbnail,
                 $request->title,
             );
-            Log::info("Thumbnail uploaded");
             if(!$thumbnailPath){
                 throw new \Exception("Failed to upload thumbnail");
             }
 
-            Log::info("Thumbnail uploaded");
             $course=Course::create([
                 "teacher_id"=>$teacher->id,
                 "category_id"=>$request->category_id,
@@ -88,8 +80,6 @@ class coursesController extends Controller
                 "price"=>$request->price,
             ]);
 
-            Log::info("Course created");
-
             foreach ($request->sections as $sectionData) {
                 $section=CourseSection::create([
                     "course_id"=>$course->id,
@@ -97,17 +87,13 @@ class coursesController extends Controller
                     "order"=>$sectionData['order'],
                 ]);
 
-                Log::info("Section created");
-
                 foreach ($sectionData['materials'] as $materialData) {
-                    Log::info("start uploading materials ");
                     $materialPath=$storage->uploadSectionMaterials(
                         $materialData['file'],
                         $course->title,
                         $section->title,
                         $materialData['title']
                     );
-                    Log::info("Material uploaded");
 
                     CourseMaterial::create([
                         "section_id"=>$section->id,
@@ -116,17 +102,14 @@ class coursesController extends Controller
                         "type"=>$materialData['type'],
                         "size"=>$materialData['size'],
                     ]);
-                    Log::info("Material created");
                 }
             }
-            Log::info("All materials uploaded");
             // make course published after successfully created
             $course->status="published";
             $course->save();
 
             return $course;
         });
-        Log::info("Exited transaction");
 
         return response()->json([
             "success"=>true,
